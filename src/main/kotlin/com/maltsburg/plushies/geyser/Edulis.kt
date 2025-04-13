e34aa0839e9f9fae43f4c156397fa0acbef3017e
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2025-04-13 00:54:24 -0700

    create music discs, fix some things

--- src/main/kotlin/com/maltsburg/plushies/geyser/Edulis.kt
+++ src/main/kotlin/com/maltsburg/plushies/geyser/Edulis.kt
@@ -1,30 +1,9 @@
 package com.maltsburg.plushies.geyser
 
 import co.akoot.plugins.edulis.Edulis.Companion.headConfig
-import co.akoot.plugins.edulis.util.Materials.resolvedResults
-import com.maltsburg.plushies.geyser.util.Helpers.register
 import org.geysermc.geyser.api.event.lifecycle.GeyserDefineCustomSkullsEvent
 
 object Edulis {
-
-    fun items(event: GeyserItemsEvent) {
-        // custom model data has to be set in order or everything breaks
-        // this is such a caveman moment for minecraft fr fr, group by material here.
-
-        // literally going to rage quit, this took like 2 hours. maybe i am the caveman
-        for ((material, keys) in resolvedResults.entries.groupBy { it.value.type }) {
-            // now sort by custom model data
-            val sortedItems = keys.filter { it.value.itemMeta.hasCustomModelData() }
-                .sortedBy { it.value.itemMeta.customModelData }
-
-            // register that jawn
-            for (key in sortedItems) {
-                val cmd = key.value.itemMeta.customModelData
-                register(event, key.key, cmd, material.name.lowercase())
-            }
-        }
-    }
-
     fun heads(event: GeyserHeadEvent) {
         val config = headConfig
 
--- src/main/kotlin/com/maltsburg/plushies/geyser/PlushiesGeyser.kt
+++ src/main/kotlin/com/maltsburg/plushies/geyser/PlushiesGeyser.kt
@@ -1,5 +1,8 @@
 package com.maltsburg.plushies.geyser
 
+import co.akoot.plugins.edulis.util.Materials.resolvedResults
+import co.akoot.plugins.plushies.util.Items.customItems
+import com.maltsburg.plushies.geyser.util.Helpers.register
 import org.geysermc.event.subscribe.Subscribe
 import org.geysermc.geyser.api.extension.Extension
 
@@ -12,12 +15,28 @@ class PlushiesGeyser : Extension {
     fun registerItems(event: GeyserItemsEvent) {
         Plushies.plushies(event)
         Plushies.books(event)
-        Edulis.items(event)
+        items(event)
     }
 
     @Subscribe
     fun registerHeads(event: GeyserHeadEvent) {
         Plushies.heads(event)
         Edulis.heads(event)
+    }
+
+    private fun items(event: GeyserItemsEvent) {
+        // custom model data has to be set in order or everything breaks
+        // group by material here
+        for ((material, keys) in customItems.plus(resolvedResults).entries.groupBy { it.value.type }) {
+            // now sort by custom model data
+            val sortedItems = keys.filter { it.value.itemMeta.hasCustomModelData() }
+                .sortedBy { it.value.itemMeta.customModelData }
+
+            // register that jawn
+            for (key in sortedItems) {
+                val cmd = key.value.itemMeta.customModelData
+                register(event, key.key, cmd, material.name.lowercase())
+            }
+        }
     }
 }
